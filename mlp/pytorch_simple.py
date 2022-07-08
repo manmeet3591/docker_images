cfb62ac73f5c348a88260363a324e00bf031afbe
Manmeet Singh <manmeet20singh11@gmail.com>
2022-07-08 14:23:33 +0530

    Update pytorch_simple.py

--- mlp/pytorch_simple.py
+++ mlp/pytorch_simple.py
@@ -1,3 +1,4 @@
+
 """
 Optuna example that optimizes multi-layer perceptrons using PyTorch.
 
@@ -19,7 +20,7 @@ import torch.optim as optim
 import torch.utils.data
 from torchvision import datasets
 from torchvision import transforms
-import pandas as pd
+
 
 DEVICE = torch.device("cuda")
 BATCHSIZE = 128
@@ -29,22 +30,18 @@ EPOCHS = 10
 N_TRAIN_EXAMPLES = BATCHSIZE * 30
 N_VALID_EXAMPLES = BATCHSIZE * 10
 
-df = pd.read_csv('study_trials_mlp.csv')
 
-#print(df.loc[df.value.idxmax()])
-df_ = df.loc[df.value.idxmax()]
-
-def define_model():
+def define_model(trial):
     # We optimize the number of layers, hidden units and dropout ratio in each layer.
-    n_layers = df_["params_n_layers"] #  trial.suggest_int("n_layers", 1, 3)
+    n_layers = trial.suggest_int("n_layers", 1, 3)
     layers = []
 
     in_features = 28 * 28
     for i in range(n_layers):
-        out_features = df_["params_n_units_l{}".format(i)] #  trial.suggest_int("n_units_l{}".format(i), 4, 128)
+        out_features = trial.suggest_int("n_units_l{}".format(i), 4, 128)
         layers.append(nn.Linear(in_features, out_features))
         layers.append(nn.ReLU())
-        p =  df_["params_dropout_l{}".format(i)] # trial.suggest_float("dropout_l{}".format(i), 0.2, 0.5)
+        p = trial.suggest_float("dropout_l{}".format(i), 0.2, 0.5)
         layers.append(nn.Dropout(p))
 
         in_features = out_features
@@ -66,46 +63,79 @@ def get_mnist():
     )
 
     return train_loader, valid_loader
-# Generate the model.
-model = define_model().to(DEVICE)
 
-# Generate the optimizers.
-optimizer_name = df_["params_optimizer"] # trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD"])
-lr = df_["params_lr"] #  trial.suggest_float("lr", 1e-5, 1e-1, log=True)
-optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
+def objective(trial):
 
-# Get the FashionMNIST dataset.
-train_loader, valid_loader = get_mnist()
+    # Generate the model.
+    model = define_model(trial).to(DEVICE)
 
-# Training of the model.
-for epoch in range(EPOCHS):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        # Limiting training data for faster epochs.
-        if batch_idx * BATCHSIZE >= N_TRAIN_EXAMPLES:
-            break
+    # Generate the optimizers.
+    optimizer_name = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD"])
+    lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
+    optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
 
-        data, target = data.view(data.size(0), -1).to(DEVICE), target.to(DEVICE)
+    # Get the FashionMNIST dataset.
+    train_loader, valid_loader = get_mnist()
 
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-
-    # Validation of the model.
-    model.eval()
-    correct = 0
-    with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(valid_loader):
-            # Limiting validation data.
-            if batch_idx * BATCHSIZE >= N_VALID_EXAMPLES:
+    # Training of the model.
+    for epoch in range(EPOCHS):
+        model.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            # Limiting training data for faster epochs.
+            if batch_idx * BATCHSIZE >= N_TRAIN_EXAMPLES:
                 break
-            data, target = data.view(data.size(0), -1).to(DEVICE), target.to(DEVICE)
-            output = model(data)
-            # Get the index of the max log-probability.
-            pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
 
-    accuracy = correct / min(len(valid_loader.dataset), N_VALID_EXAMPLES)
-    print(accuracy)
+            data, target = data.view(data.size(0), -1).to(DEVICE), target.to(DEVICE)
+
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+
+        # Validation of the model.
+        model.eval()
+        correct = 0
+        with torch.no_grad():
+            for batch_idx, (data, target) in enumerate(valid_loader):
+                # Limiting validation data.
+                if batch_idx * BATCHSIZE >= N_VALID_EXAMPLES:
+                    break
+                data, target = data.view(data.size(0), -1).to(DEVICE), target.to(DEVICE)
+                output = model(data)
+                # Get the index of the max log-probability.
+                pred = output.argmax(dim=1, keepdim=True)
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+        accuracy = correct / min(len(valid_loader.dataset), N_VALID_EXAMPLES)
+
+        trial.report(accuracy, epoch)
+
+        # Handle pruning based on the intermediate value.
+        if trial.should_prune():
+            raise optuna.exceptions.TrialPruned()
+
+    return accuracy
+if __name__ == "__main__":
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective, n_trials=100, timeout=600)
+
+    pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
+    complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
+
+    print("Study statistics: ")
+    print("  Number of finished trials: ", len(study.trials))
+    print("  Number of pruned trials: ", len(pruned_trials))
+    print("  Number of complete trials: ", len(complete_trials))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("  Value: ", trial.value)
+
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print("    {}: {}".format(key, value))
+    df = study.trials_dataframe()
+
+    df.to_csv('study_trials_mlp.csv')
